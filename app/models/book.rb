7cf2d3a6c6d72ba5c0f4accb3fbc42d9a0cf98ff
EC2 Default User <ec2-user@ip-172-31-55-219.ec2.internal>
2021-12-01 15:13:45 +0000

    [fix]応用課題7b解答の書き方に修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -13,8 +13,8 @@ class Book < ApplicationRecord
 
 	scope :create_today, -> { where(created_at: Time.zone.now.all_day) }
 	scope :create_yesterday, -> { where(created_at: 1.day.ago.all_day) }
-	scope :create_one_week, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }
-	scope :create_one_week_ago, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day - 13.day)) }
+	scope :create_one_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+	scope :create_one_week_ago, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
 
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
